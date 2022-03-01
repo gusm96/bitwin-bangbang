b357ca2e19d35e544abeab8ea9023cf030f4d302
Seongmo Gu <77833389+gusm96@users.noreply.github.com>
2022-03-02 01:46:08 +0900

    Update AdminLoginController

--- BangBang/src/main/java/com/bitwin/bangbang/admin/controller/AdminLoginController.java
+++ BangBang/src/main/java/com/bitwin/bangbang/admin/controller/AdminLoginController.java
@@ -8,24 +8,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bitwin.bangbang.admin.domain.Admin;
 import com.bitwin.bangbang.admin.service.AdminLoginService;
 import com.bitwin.bangbang.exception.LoginInvalidException;
 
 @Controller
-@RequestMapping("/login/admin")
+
 public class AdminLoginController {
 	
 	@Autowired
 	private AdminLoginService loginService;
-	
-	@GetMapping
+	@RequestMapping(value = "/login/admin" ,method = RequestMethod.GET)
 	public String getLogin() {
 		return "admin/login";
 	}
-	@PostMapping
+	@RequestMapping(value = "/login/admin" ,method = RequestMethod.POST)
 	public String postLogin(Admin loginReq, HttpServletResponse res, HttpSession session) throws LoginInvalidException {
 		return loginService.login(loginReq ,res, session);
+	}
+	@RequestMapping(value = "/logout/admin" ,method = RequestMethod.GET)
+	public String getLogout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/main/mainpage";
 	}
 }
