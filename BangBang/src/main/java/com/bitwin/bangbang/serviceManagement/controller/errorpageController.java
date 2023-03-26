5b8e125a61c847a9a9a9537c888b77cb4068b95c
Seongmo Gu <77833389+gusm96@users.noreply.github.com>
2023-03-27 00:07:10 +0900

    Update 230327

--- BangBang/src/main/java/com/bitwin/bangbang/serviceManagement/controller/errorpageController.java
+++ BangBang/src/main/java/com/bitwin/bangbang/serviceManagement/controller/errorpageController.java
@@ -14,20 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Handles requests for the application home page.
  */
 @Controller
-@RequestMapping("/error/*")
+@RequestMapping("error")
 public class errorpageController {
 	
 	@GetMapping("/errorpage")
-	public void errorpage() {
-		
-
-		
-	}
-	
-	
-	@RequestMapping(value="/errorpage")
-	public String error(HttpServletRequest request) {
+	public String errorpage() {
 		return "/bangbang/error";
 	}
-	
 }
